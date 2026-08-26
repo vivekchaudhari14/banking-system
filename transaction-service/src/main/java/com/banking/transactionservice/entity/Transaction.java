618057783b2729a5cf22d7d70bb9db8594afa937
vivekchaudhari14 <vivekchaudhari14@zohomail.in>
2026-08-26 16:51:35 +0530

    Transaction entity done

---
+++ transaction-service/src/main/java/com/banking/transactionservice/entity/Transaction.java
@@ -0,0 +1,57 @@
+package com.banking.transactionservice.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="transactions")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Transaction {
+
+    @id(strategy = GenerationType.UUID)
+    String id;
+
+    @Column(nullable = false)
+    String senderAccountNumber;
+
+    @Column(nullable = false)
+    String recipientAccountNumber;
+
+    @Column(nullable = false, precision = 15, scale = 2)
+    BigDecimal amount;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    TransactionType type;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    TransactionStatus status;
+
+    String description;
+
+    String failureReason;
+
+    String referenceNumber;
+
+    @CreationTimestamp
+    LocalDateTime createdAt;
+
+
+    LocalDateTime completedAt;
+
+
+
+}
